3025b051e42d47d02229fdd0b0420cc07f159fe5
RubenBenavidess <dario.benavides@hotmail.com>
2025-08-07 11:51:19 -0500

    Proyecto base con CI

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+
+// Endpoint
+
+app.get('/', (_req, res) => {
+    res.send('Integración continua funcionando!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
